f180ef60a6b2de106d7c1fd3790ffde59d12c733
Paulo Sergio dos Santos Araujo <paulorugal@gmail.com>
2014-10-23 16:25:24 -0400

    same as before

---
+++ pendulum/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	// Custom Javascript for the Pendulum Assignment. 
+	// 
+	// If you are going to alter this file please follow
+	// this. 
+	//
+	//
+	// 
+	// .. function declarations ..
+	// .. class declarations .. 
+	// main() declaration
+	// main();
+
+	function theta(int inputs){
+		// TODO
+	}
+
+	function validateInput(){
+		var fa = $("#firstAngle").val();
+		var sa = $("#secondAngle").val();
+
+		try {
+
+		} catch (err) {
+			alert("The angles have to be integers.");
+		}
+	}
+
+	function main(){
+
+	}
+
+
+	main();
+
+});
